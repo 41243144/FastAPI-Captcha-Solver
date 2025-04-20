74aa10cee2b651f5c6fadf1e807a520b7be142b8
41243144 <41243144@nfu.edu.tw>
2025-04-20 15:07:15 +0800

    FastAPI Server create

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import ddddocr
 
 app = FastAPI()
 ocr = ddddocr.DdddOcr()
+
+# 配置 CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/predict")
 async def predict_captcha(file: UploadFile = File(...)):
